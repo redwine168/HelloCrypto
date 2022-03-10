d9bc3fd91cc29191b7009d0fa582ebb44c9e464d
Jonathan Redwine <jredwine1@gmail.com>
2022-03-10 15:58:14 -0500

    updated server listen in app

--- server/app.js
+++ server/app.js
@@ -77,6 +77,9 @@ io.on('connection', (socket) => {
 });
 
 
+/*
 server.listen(port, hostname, () => {
      console.log(`Server running at http://${hostname}:${port}/`);
-});
+});*/
+
+server.listen(process.env.port);
